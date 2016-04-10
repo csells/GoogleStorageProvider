07da57a5b362ec78a04287a17d8179afb92e641d
Chris Sells <csells@sellsbrothers.com>
2016-04-10 15:20:45 -0700

    updated to use the apiary client
    hopefully this is temporary

--- StorageProvider/StorageProvider.cs
+++ StorageProvider/StorageProvider.cs
@@ -1,5 +1,6 @@
 ﻿// from https://msdn.microsoft.com/en-us/library/dn727071(v=vs.85).aspx
-using Google.Storage.V1;
+using Google.Apis.Services;
+using Google.Apis.Storage.v1;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -30,16 +31,33 @@ namespace StorageProvider {
       return base.ItemExists(path);
     }
 
+    public StorageService CreateStorageClient() {
+      var credentials = Google.Apis.Auth.OAuth2.GoogleCredential.GetApplicationDefaultAsync().Result;
+
+      if (credentials.IsCreateScopedRequired) {
+        credentials = credentials.CreateScoped(new[] { StorageService.Scope.DevstorageFullControl });
+      }
+
+      var serviceInitializer = new BaseClientService.Initializer() {
+        ApplicationName = "Google Cloud Storage PowerShell Provider",
+        HttpClientInitializer = credentials
+      };
+
+      return new StorageService(serviceInitializer);
+    }
+
     // dir gcs:/
     // dir gcs:\
     protected override void GetChildItems(string path, bool recurse) {
       Debug.WriteLine($"GetChildItems: path= {path}, recurse= {recurse}");
 
       // TODO: put client and projectId into properties that do the right thing
-      var client = StorageClient.FromApplicationCredentials("PowerShell-StorageProvider").Result;
+      //var client = StorageClient.FromApplicationCredentials("PowerShell-StorageProvider").Result;
       var projectId = "firm-site-126023";
 
-      foreach( var bucket in client.ListBuckets(projectId)) { WriteItemObject(bucket, $"gcs:/{bucket.Name}", true); }
+      //foreach( var bucket in client.ListBuckets(projectId)) { WriteItemObject(bucket, $"gcs:/{bucket.Name}", true); }
+      var client = CreateStorageClient();
+      foreach( var bucket in client.Buckets.List(projectId).Execute().Items ) { WriteItemObject(bucket, $"gcs:/{bucket.Name}", true); }
     }
 
   }
